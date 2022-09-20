4cbe1948f82984220547112aa112bca8b18844fd
Vicram <vicramanjohfue@gmail.com>
2022-09-20 09:14:26 +0300

    Aws Cloud Development Kit (CDK)

--- lib/awscdk-stack.ts
+++ lib/awscdk-stack.ts
@@ -1,15 +1,29 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
 // import * as sqs from '@aws-cdk/aws-sqs';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 
 export class AwscdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", { maxAzs: 2 });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwscdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new ecs.Cluster(this, "MyCluster", { vpc: vpc });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "MyFargateService",
+      {
+        cluster: cluster,
+
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+        },
+
+        publicLoadBalancer: true,
+      }
+    );
   }
 }
